f2195534ae6258120accee671b85231168f289bb
Halit Sever <34791459+halitsever@users.noreply.github.com>
2021-07-02 10:59:49 +0300

    dark mode added

--- contents/scripts/main.js
+++ contents/scripts/main.js
@@ -1,9 +1,3 @@
-var cards = document.querySelector("#cards");
-
-function openMenu() {
-
-}
-
 fetch("https://api.github.com/users/halitsever/repos?sort=updated")
 .then((response) => response.json())
 .then((data) => {
@@ -25,3 +19,58 @@ fetch("https://api.github.com/users/halitsever/repos?sort=updated")
   }
 });
 
+
+
+
+var cards = document.querySelector("#cards");
+var body = document.querySelector("body");
+var header = document.querySelector(".header");
+var lista = document.querySelectorAll("a");
+var listh = document.querySelectorAll("h2");
+var listp = document.querySelectorAll("p");
+function lightMode() {
+for (var i = 0; lista.length > lista.length - 1; i++) {
+  console.log(i);
+  lista[i].style.color = "black";
+  if (i === lista.length - 1) break;
+}
+for (var i = 0; listh.length > listh.length - 1; i++) {
+  console.log(i);
+  listh[i].style.color = "black";
+  if (i === listh.length - 1) break;
+}
+for (var i = 0; listp.length > listp.length - 1; i++) {
+  console.log(i);
+  listp[i].style.color = "black";
+  if (i === listp.length - 1) break;
+}
+body.style.backgroundColor = "whitesmoke";
+header.style.backgroundColor = "#dbdbdb";
+}
+
+
+
+function darkMode() {
+  for (var i = 0; lista.length > lista.length - 1; i++) {
+    console.log(i);
+    lista[i].style.color = "white";
+    if (i === lista.length - 1) break;
+  }
+  for (var i = 0; listh.length > listh.length - 1; i++) {
+    console.log(i);
+    listh[i].style.color = "white";
+    if (i === listh.length - 1) break;
+  }
+  for (var i = 0; listp.length > listp.length - 1; i++) {
+    console.log(i);
+    listp[i].style.color = "white";
+    if (i === listp.length - 1) break;
+  }
+  body.style.backgroundColor = "#393e46";
+  header.style.backgroundColor = "#222831";
+  }
+
+
+function openMenu() {
+
+}
